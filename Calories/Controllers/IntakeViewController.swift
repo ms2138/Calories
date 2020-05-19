51480998c2261201fa89ee62685a03f807421d2c
ms2138 <m38371@gmail.com>
2020-05-18 21:08:54 -0700

    Add IntakeViewController class

---
+++ Calories/Controllers/IntakeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  IntakeViewController.swift
+//  Calories
+//
+//  Created by mani on 2020-05-18.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class IntakeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
