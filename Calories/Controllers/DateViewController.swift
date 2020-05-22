6ad6585e07a6555ccb9dbfc8e7e310a6dc6d8eb1
ms2138 <m38371@gmail.com>
2020-05-21 21:38:47 -0700

    Add DateViewController class

---
+++ Calories/Controllers/DateViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DateViewController.swift
+//  Calories
+//
+//  Created by mani on 2020-05-21.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class DateViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
