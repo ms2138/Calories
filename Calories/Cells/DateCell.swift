a11204169357486b0ee1e62efe8df46c0b560723
ms2138 <m38371@gmail.com>
2020-05-23 15:38:15 -0700

    Add DateCell class.  Add Cells folder.  Move DateCell to Cells folder

---
+++ Calories/Cells/DateCell.swift
@@ -0,0 +1,12 @@
+//
+//  DateCell.swift
+//  Calories
+//
+//  Created by mani on 2020-05-23.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class DateCell: UITableViewCell {
+}
