7b4c139c5fd9a7a6a44c156f630d9501e82774ad
ms2138 <m38371@gmail.com>
2020-05-24 15:26:09 -0700

    Add backgroundView property.  Set backgroundView to tableView's backgroundView property.  Add protocol conformance for NoContentBackgroundView in CaloriesViewController

--- Calories/Controllers/CaloriesViewController.swift
+++ Calories/Controllers/CaloriesViewController.swift
@@ -9,13 +9,25 @@
 import UIKit
 import CoreData
 
-class CaloriesViewController: UIViewController {
+class CaloriesViewController: UIViewController, NoContentBackgroundView {
     
     @IBOutlet weak var tableView: UITableView!
     var intakeRecord: Intake?
     var managedObjectContext: NSManagedObjectContext?
+    lazy var backgroundView: DTTableBackgroundView = {
+        let backgroundView = DTTableBackgroundView(frame: self.view.frame)
+        backgroundView.messageLabel.text = "Please add calories"
+        backgroundView.buttonTitle = "Add"
+        backgroundView.handler = {
+            self.performSegue(withIdentifier: "showAddCalorie", sender: nil)
+        }
+        return backgroundView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.backgroundView = backgroundView
+        hideBackgroundView()
     }
 }
