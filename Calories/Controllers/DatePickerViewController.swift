1556a37259f4df9d8ffaf03bf382fee70338281a
ms2138 <m38371@gmail.com>
2020-05-24 22:58:54 -0700

    Add saveBarButtonItem IBOutlet property and dateChanged method to DatePickerViewController.  Add functionality to enable the save button if the date has not been changed by the user

--- Calories/Controllers/DatePickerViewController.swift
+++ Calories/Controllers/DatePickerViewController.swift
@@ -12,6 +12,7 @@ class DatePickerViewController: UITableViewController {
     @IBOutlet weak var dateCell: DateCell!
     var dateChangedHandler: ((Date) -> Void)?
     var currentDate: Date?
+    @IBOutlet weak var saveBarButtonItem: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +22,17 @@ class DatePickerViewController: UITableViewController {
         if let date = currentDate {
             dateCell.datePicker.date = date
         }
-    }
 
+        dateCell.datePicker.addTarget(self, action: #selector(dateChanged(_:)), for: .valueChanged)
+    }
+}
+
+extension DatePickerViewController {
+    @objc func dateChanged(_ sender: UIDatePicker) {
+        if let currentDate = currentDate {
+            saveBarButtonItem.isEnabled = currentDate != sender.date
+        }
+    }
 }
 
 extension DatePickerViewController {
