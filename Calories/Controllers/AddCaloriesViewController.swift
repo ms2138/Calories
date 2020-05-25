0036d73d320a5e14116970fdcaba486cff415abb
ms2138 <m38371@gmail.com>
2020-05-24 19:30:01 -0700

    Add AddCaloriesViewController class

---
+++ Calories/Controllers/AddCaloriesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddCaloriesViewController.swift
+//  Calories
+//
+//  Created by mani on 2020-05-24.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class AddCaloriesViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
